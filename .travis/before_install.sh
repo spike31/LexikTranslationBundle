c7967edc8e02ad1a2533a7283728677d7b8124ec
Gauthier Gilles <g.gauthier@lexik.fr>
2020-05-13 13:44:53 +0200

    up

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -9,15 +9,13 @@ PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
 TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
 echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
-#if [ "$TRAVIS_PHP_VERSION" '<' '7.0' ]; then
-#    echo "extension=mongo.so" >> "$TRAVIS_INI_FILE"
-#else
-#    echo "extension=mongodb.so" >> "$TRAVIS_INI_FILE"
-#
-#    # Backwards compatibility with old mongo extension
-#    composer require "alcaeus/mongo-php-adapter" --no-update
-#fi
-#
-#composer require "symfony/symfony:${SYMFONY_VERSION}" --no-update
+if [ "$TRAVIS_PHP_VERSION" '<' '7.0' ]; then
+    echo "extension=mongo.so" >> "$TRAVIS_INI_FILE"
+else
+    echo "extension=mongodb.so" >> "$TRAVIS_INI_FILE"
 
-exit 0;
+    # Backwards compatibility with old mongo extension
+    composer require "alcaeus/mongo-php-adapter" --no-update
+fi
+
+composer require "symfony/symfony:${SYMFONY_VERSION}" --no-update
